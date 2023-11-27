d87c842dfe193e8092bd56cb4256a35b8bb0c20b
zbornak <zbornak@protonmail.com>
2023-11-27 10:57:35 +0000

    Moved handler functions to separate go file

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,48 +7,9 @@ Date: 27/11/2023
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 )
-
-// handlers
-func home(w http.ResponseWriter, r *http.Request) {
-	// 404 if URL path doesn't match "/"
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-
-	w.Write([]byte("Welcome to turboPay"))
-}
-
-// view stock item
-func itemView(w http.ResponseWriter, r *http.Request) {
-	// allow for user id item query, checking to make sure user enters a valid uint
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
-		return
-	}
-
-	fmt.Fprintf(w, "Display a specific stock item with ID %d...", id)
-}
-
-// create new stock item
-func itemCreate(w http.ResponseWriter, r *http.Request) {
-	// method not allowed (405) if request method isn't POST
-	if r.Method != http.MethodPost {
-		// add 'Allow:POST' to response header map to let user know what request is allowed
-		w.Header().Set("Allow", "POST")
-
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	w.Write([]byte("Create a new stock item..."))
-}
 
 func main() {
 	// router
