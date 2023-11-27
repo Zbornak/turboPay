fafd9876cbf41688567d28f958f569f2b1cc4c26
zbornak <zbornak@protonmail.com>
2023-11-27 09:18:36 +0000

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module turboPay.zbornak
+
+go 1.21.4
---
+++ main.go
@@ -0,0 +1,15 @@
+/*
+A web point-of-sales application
+Author: Mark Strijdom (zbornak)
+Date: 27/11/2023
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+}
