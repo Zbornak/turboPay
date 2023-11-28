f76aa508ffd4bce4ab4c77b059c93e5a8188e810
zbornak <zbornak@protonmail.com>
2023-11-28 10:50:37 +0000

    Added custom info/err logs

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -10,12 +10,19 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	// flag allowing user to decide port (:4000 default)
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
+
+	// logger for informational messages
+	infoLog := log.New(os.Stdout, "INFO:\t", log.Ldate|log.Ltime)
+
+	// logger for error messages
+	errorLog := log.New(os.Stderr, "ERROR:\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	// router
 	mux := http.NewServeMux()
@@ -31,9 +38,12 @@ func main() {
 	mux.HandleFunc("/stockItem/view", itemView)
 	mux.HandleFunc("/stockItem/create", itemCreate)
 
-	log.Printf("Starting server on port %s...", *addr)
+	// infoLog handles info message
+	infoLog.Printf("Starting server on port %s...", *addr)
 
 	// start new web server on port 4000
 	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+
+	//errorLog handles error message (if any)
+	errorLog.Fatal(err)
 }
